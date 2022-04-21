f8455baf35f2f1c689e14adf04af533896b07834
Zach Cullen <zacullen@gmail.com>
2022-04-21 11:27:09 -0500

    create list view endpoint for posts

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Post
+from .serializers import PostSerializer
+
+class PostList(generics.ListCreateAPIView):
+  queryset = Post.objects.all()
+  serializer_class = PostSerializer
